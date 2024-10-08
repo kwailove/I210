9acd5886b8be94ae358675f3b0d7c5c6ab65b955
kwailove <kwailove@iu.edu>
2024-10-08 10:49:02 -0400

    Js Variable Basics: Went through making JS Variables

---
+++ week7/app.js
@@ -0,0 +1,61 @@
+// ways to create variables
+// 1. using var makes a variable that can be reassigned and globally referenced
+var myName = "Kwai";
+
+// 2. Using let makes variables that you can change the value
+
+let myTitle = "The Kangaroo Guy";
+
+// life event
+myTitle = "the Crocodile Guy";
+
+// 3. Using const variables cannot change their value. const is short for constant.
+
+const ssn = 123;
+
+// quotation marks always means that it is a striing.
+
+// Variable types
+// Primitive variable types
+// 1. Strings
+let str1 = "Something";
+const str2 = "123";
+// Starting quotes must match ending quote
+const str4 = `string that can be broken into multiple lines`;
+
+// 2. Number
+let myAge = 52;
+
+const oneLessThanZero = -1;
+let numberOfKidsThatIHave = 2.33;
+
+// 3. Boolean
+// True and False : 1 or 0
+let amIBald = true;
+const isTheSunKindaFar = false;
+
+// Complex Variables
+
+//  4. Null / Undefined
+// Null means you defined it but did not give it a value
+// Undefined means you did not define it at all
+
+let myCar;
+
+// 5. Arrays - list of stuff
+
+cosnt myFavoriteColors = ["pink", "blue", "gray"];
+const randomStuff = [true, 1, "blah",  -200];
+
+// 6. Objects - variables that you need to describe
+const person { 
+    // property of the object 
+    hairColor: "black",
+// methods are like functions that belong to the object
+sayMyName () {},
+};
+
+// 7. Functions
+function doSomething () {
+    // Make your function something
+}
